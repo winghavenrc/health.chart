798010eb46518786c778db1e9ae5b8603a35006b
winghavenrc <99767144+winghavenrc@users.noreply.github.com>
2023-02-22 15:24:06 -0600

    r

--- __init__.py
+++ __init__.py
@@ -33,59 +33,69 @@ class HealthChart(MycroftSkill):
             self.speak_dialog('main.menu', expect_response=False)
 # Opening JSON file
         else:
-            self.log.info(self.file_system.path)
 
-            with self.file_system.open('care_team.json', "r") as care_team_file:
-                #            f = open('care_team.json')
+            self.provider_list = []
 
-                # returns JSON object as
-                # a dictionary
-                care_team = json.load(care_team_file)
-
-#            (data['people1'][0]
-# Iterating through the json
-# list
-
-    # for printing the key-value pair of
-    # nested dictionary for loop can be used
-
-                self.provider_list = []
-
-                for provider in care_team['entry']:
-                    name = provider['name']
-                    specialty = provider['specialty']
-                    self.log.info(name)
-                    self.log.info(specialty)
-
-#                    lastname = provider['name']['family'];
-#                    firstname = name['given'];
-#                    fullname = firstname + " " + lastname + " " + specialty;
-#                    self.provider_list.append(fullname)
-                    self.provider_list.append(specialty)
-
-                care_team_file.close()
+            if get_care_team(self.provider_list) is True:
 
                 self.log.info(self.provider_list)
                 self.speak_dialog(
                     "I can schedule with any of your currently active providers. Which one of these do you want to schedule with...", wait=True)
 
                 selected = self.ask_selection(self.provider_list)
-                self.speak_dialog('get.provider', data={"provider": selected}, expect_response=True, wait=True)
+                self.speak_dialog('get.provider', data={
+                                  "provider": selected}, expect_response=True, wait=True)
 
 #               find first appointments available from today
                 timeSlots = find_first(self)
                 self.speak_dialog('speak.times', data={"total": len(
                     timeSlots)}, expect_response=False, wait=False)
                 for index in range(0, len(timeSlots)):
-                    self.speak_dialog('speak.timeslots', data={"slot": timeSlots[index]["start"]}, expect_response=False, wait=False)
-
+                    self.speak_dialog('speak.timeslots', data={
+                                      "slot": timeSlots[index]["start"]}, expect_response=False, wait=False)
 
     def stop(self):
         pass
 
 
+def get_care_team(self):
+
+  self.log.info(self.file_system.path)
+
+  with self.file_system.open('care_team.json', "r") as care_team_file:
+#            f = open('care_team.json')
+
+# returns JSON object as
+# a dictionary
+    care_team = json.load(care_team_file)
+
+#            (data['people1'][0]
+# Iterating through the json
+# list
+
+# for printing the key-value pair of
+# nested dictionary for loop can be used
+
+    for provider in care_team['entry']:
+      name = provider['name']
+      specialty = provider['specialty']
+      self.log.info(name)
+      self.log.info(specialty)
+
+#                    lastname = provider['name']['family'];
+#                    firstname = name['given'];
+#                    fullname = firstname + " " + lastname + " " + specialty;
+#                    self.provider_list.append(fullname)
+      self.provider_list.append(specialty)
+
+    care_team_file.close()
+    return True
+
+  return False
+
+
 def find_first(self):
-    
+
     # get the current date and time
     today = datetime.date.today()
 
@@ -96,139 +106,139 @@ def find_first(self):
 
     searchDate = today
 
-    for day in range(1,5):
+    for day in range(1, 5):
 
-      availableTimes = mt_find_available_appts(self, searchDate, 'pm', 'America/Chicago')
-      if len(availableTimes) > 0:
-        self.log.info(searchDate)
+        availableTimes = mt_find_available_appts(
+            self, searchDate, 'pm', 'America/Chicago')
+        if len(availableTimes) > 0:
+            self.log.info(searchDate)
 #               meditech.revokeToken(handlerInput); // see revokeToken for why to call this now
-        break
-       
-      searchDate = datetime.date(searchDate.year, searchDate.month, searchDate.day+1)
-    
+            break
+
+        searchDate = datetime.date(
+            searchDate.year, searchDate.month, searchDate.day+1)
 
     return availableTimes
 
 
-
 def mt_find_available_appts(self, searchDate, ampm, userTimezone):
 
-# for a given searchDate
+    # for a given searchDate
 
-  availableTimes = []
+    availableTimes = []
 
-  ### Get a Meditech token
+    # Get a Meditech token
 
-  BASE_HOST = 'https://greenfield-apis.meditech.com'
-  IMPLEMENTATION_VERSION = '/v1/argoScheduling/STU3'
-  OAUTH_AUTHORIZE = '/oauth/authorize'
-  OAUTH_TOKEN = '/oauth/token'
-  CLIENT_ID = 'Voxhealth@8c76706c946d4426a648b5c2789cd7e1'
-  CLIENT_SECRET = 'gT_463aNSJiGJA7jvsCM4g=='
-  GRANT_TYPE = 'client_credentials'
-  SCOPE = 'patient/ArgoScheduling.* patient/ArgoScheduling.read'
-  BASE_APPOINTMENT = '/Appointment'
-  FIND_APPOINTMENTS = '/$find'
-  HOLD_APPOINTMENT = '/$hold'
-  BOOK_APPOINTMENT = '/$book'
-
+    BASE_HOST = 'https://greenfield-apis.meditech.com'
+    IMPLEMENTATION_VERSION = '/v1/argoScheduling/STU3'
+    OAUTH_AUTHORIZE = '/oauth/authorize'
+    OAUTH_TOKEN = '/oauth/token'
+    CLIENT_ID = 'Voxhealth@8c76706c946d4426a648b5c2789cd7e1'
+    CLIENT_SECRET = 'gT_463aNSJiGJA7jvsCM4g=='
+    GRANT_TYPE = 'client_credentials'
+    SCOPE = 'patient/ArgoScheduling.* patient/ArgoScheduling.read'
+    BASE_APPOINTMENT = '/Appointment'
+    FIND_APPOINTMENTS = '/$find'
+    HOLD_APPOINTMENT = '/$hold'
+    BOOK_APPOINTMENT = '/$book'
 
 
 # Define the OAuth2 client ID and secret
-  client_id = CLIENT_ID
-  client_secret = CLIENT_SECRET
+    client_id = CLIENT_ID
+    client_secret = CLIENT_SECRET
 
 # Define the OAuth2 token endpoint
-  token_url = BASE_HOST + OAUTH_TOKEN
+    token_url = BASE_HOST + OAUTH_TOKEN
 
 # Request an access token
 
+    token_req_payload = {'grant_type': 'client_credentials',
+                         'scope': 'patient/ArgoScheduling.* patient/ArgoScheduling.read'}
 
-  token_req_payload = {'grant_type': 'client_credentials', 'scope': 'patient/ArgoScheduling.* patient/ArgoScheduling.read'}
-
-  response = requests.post(token_url, data=token_req_payload, verify=False, allow_redirects=False, auth=(client_id, client_secret))
+    response = requests.post(token_url, data=token_req_payload, verify=False,
+                             allow_redirects=False, auth=(client_id, client_secret))
 
 #  response = requests.post(token_url, data={"grant_type": "client_credentials", "client_id": client_id, "client_secret": client_secret })
-  self.log.info(response)
+    self.log.info(response)
 
 # Parse the JSON response
-  response_data = json.loads(response.text)
+    response_data = json.loads(response.text)
 
 # Extract the access token from the response
-  access_token = response_data["access_token"]
+    access_token = response_data["access_token"]
 
 # Define the API endpoint
-  url = BASE_HOST + IMPLEMENTATION_VERSION + BASE_APPOINTMENT + FIND_APPOINTMENTS
+    url = BASE_HOST + IMPLEMENTATION_VERSION + BASE_APPOINTMENT + FIND_APPOINTMENTS
 
 # Set the Authorization header with the access token as a bearer token
-  headers = {
-    "Authorization": "Bearer " + access_token
-  }
+    headers = {
+        "Authorization": "Bearer " + access_token
+    }
 
-  begin = searchDate
-  end = datetime.date(searchDate.year, searchDate.month, searchDate.day+1)
+    begin = searchDate
+    end = datetime.date(searchDate.year, searchDate.month, searchDate.day+1)
 
 # Define the parameters as a dictionary
-  params = {
-        "practitioner": '5563b254-66b1-5203-80e3-bef0be824970',  #Meehan
-        "location": '1b2332fb-8906-5264-86e0-df72e983f350',  #Cardiology
-        'service-type': '257585005',    #ECHO
-        "start": begin,
-        "end": end
-  }
+    params = {
+        "practitioner": '5563b254-66b1-5203-80e3-bef0be824970',  # Meehan
+        "location": '1b2332fb-8906-5264-86e0-df72e983f350',  # Cardiology
+          'service-type': '257585005',  # ECHO
+          "start": begin,
+          "end": end
+    }
 
 # Make a GET request to the API
-  response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers, params=params)
 
 # Check if the request was successful
-  if response.status_code == 200:
-    # Parse the JSON data returned by the API
-    apptSlots = response.json()
-    self.log.info(apptSlots)
+    if response.status_code == 200:
+        # Parse the JSON data returned by the API
+        apptSlots = response.json()
+        self.log.info(apptSlots)
 
-    total = apptSlots["total"];
-    if total == 0:
-        # means there's no appointments available
-      return availableTimes
+        total = apptSlots["total"]
+        if total == 0:
+            # means there's no appointments available
+            return availableTimes
 
-    start = []
-    id = []
+        start = []
+        id = []
 
-  
-    for index in range(0,total):
+        for index in range(0, total):
 
-      self.log.info(apptSlots["entry"][index]["resource"])
+            self.log.info(apptSlots["entry"][index]["resource"])
 
-      localStart_dt = datetime.datetime.strptime(apptSlots["entry"][index]["resource"]["start"], "%Y-%m-%dT%H:%M:%S%z")
-      self.log.info(localStart_dt)
-      meridien = localStart_dt.strftime("%p")
-      localStart_str = datetime.datetime.strftime(localStart_dt, "%A %B %-d %-I:%-M %p")
+            localStart_dt = datetime.datetime.strptime(
+                apptSlots["entry"][index]["resource"]["start"], "%Y-%m-%dT%H:%M:%S%z")
+            self.log.info(localStart_dt)
+            meridien = localStart_dt.strftime("%p")
+            localStart_str = datetime.datetime.strftime(
+                localStart_dt, "%A %B %-d %-I:%-M %p")
 
-      save = False
+            save = False
 
-      if ampm in ["am", "AM", "MO", "morning"]:
-        if meridien == "AM":
-            save = True
-      elif ampm in ["pm", "PM", "AF", "afternoon"]:
-        if meridien == "PM":
-            save = True
-      if save == True:
-        slot = {"start": localStart_str, "id": apptSlots["entry"][index]["resource"]["id"]}
-        availableTimes.append(slot)
+            if ampm in ["am", "AM", "MO", "morning"]:
+                if meridien == "AM":
+                    save = True
+            elif ampm in ["pm", "PM", "AF", "afternoon"]:
+                if meridien == "PM":
+                    save = True
+            if save == True:
+                slot = {"start": localStart_str,
+                        "id": apptSlots["entry"][index]["resource"]["id"]}
+                availableTimes.append(slot)
 #        start.append(localStart);
 #        id.append(apptSlots["entry"][index]["resource"]["id"]);
-  
+
 #    availableTimes = { "start": start, "id": id }
-    self.log.info(availableTimes)
+        self.log.info(availableTimes)
 
-  else:
-    # Handle error
-    self.log.info(response.status_code)
+    else:
+        # Handle error
+        self.log.info(response.status_code)
 
-
-  return availableTimes
+    return availableTimes
 
 
 def create_skill():
     return HealthChart()
-
